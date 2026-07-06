017fc30edef67a7e73db1e2725f5639baa8b0fbe
akkaziev_aktan <salizardfnite@gmail.com>
2026-07-06 15:17:06 +0600

    added follow model

---
+++ instagram/models/follow.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 related_name="following", on_delete=models.CASCADE,)
+    following = models.ForeignKey(settings.AUTH_USER_MODEL,
+        related_name="followers",
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["follower", "following"], name="unique_follow"
+            )
+        ]
